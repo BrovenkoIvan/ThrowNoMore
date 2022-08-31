f77b30c510d801778cae83cd6ea2bced6911008a
Ivan Work <ivanwork@MacBook-Pro-Ivan-2.local>
2022-08-31 18:05:30 +0300

    .

--- src/screens/Notifications/NotificationsScreen.tsx
+++ src/screens/Notifications/NotificationsScreen.tsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { View, useWindowDimensions, TouchableOpacity, Text } from 'react-native';
 import { Header } from '../components/Header';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import LinearGradient from 'react-native-linear-gradient';
+import { Colors } from '../../styles';
 
-const FirstRoute = () => <View style={{ flex: 1, backgroundColor: '#ff4081' }} />;
+const FirstRoute = () => <View style={{ flex: 1, backgroundColor: 'white' }} />;
 
-const SecondRoute = () => <View style={{ flex: 1, backgroundColor: '#673ab7' }} />;
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: '#636363' }} />;
 
 const renderScene = SceneMap({
   first: FirstRoute,
@@ -32,19 +34,37 @@ const NotificationsScreen: React.FC = () => {
         renderTabBar={(props) => (
           <TabBar
             {...props}
-            renderTabBarItem={() => {
+            renderIndicator={() => null}
+            renderTabBarItem={(props) => {
+              console.log('props', JSON.stringify(props, null, 2));
+              const { route } = props;
               return (
-                <TouchableOpacity style={{ flex: 0.5 }}>
-                  <Text>1</Text>
+                <TouchableOpacity
+                  style={{ height: 50, flex: 0.5 }}
+                  activeOpacity={0.9}
+                  key={props.key}
+                >
+                  <LinearGradient
+                    colors={[Colors.Gradient_1, Colors.Gradient_2]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                    style={{ width: '100%', height: '100%' }}
+                  >
+                    <Text>{route.title}</Text>
+                  </LinearGradient>
                 </TouchableOpacity>
               );
             }}
-            style={{ borderBottomLeftRadius: 24, borderBottomRightRadius: 24 }}
+            style={{
+              borderBottomLeftRadius: 24,
+              borderBottomRightRadius: 24,
+              backgroundColor: 'white',
+            }}
           />
         )}
       />
     </>
   );
 };
-// {/*// renderTabBar={(props) => <RenderTabBar {...props} />}*/}
+
 export default NotificationsScreen;
--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,8 @@
 export const Colors = {
   MAIN: '#60923e',
-  DARKGREY: "#A9A9A9",
-  BlueGrey: "#d0dce2",
-  LightMain: 'rgba(221, 235, 202, 1)'
+  DARKGREY: '#A9A9A9',
+  BlueGrey: '#d0dce2',
+  LightMain: 'rgba(221, 235, 202, 1)',
+  Gradient_1: 'rgba(106,147, 62, 1)',
+  Gradient_2: 'rgba(147,193, 95, 1)',
 };
